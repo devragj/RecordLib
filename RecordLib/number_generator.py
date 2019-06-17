305e117d425770db566bd56d0b4823aa8614b75f
Nate Vogel <nathan.c.vogel@gmail.com>
2019-06-17 11:57:44 -0400

    added a script, callable using setup.py's entrypoints, to download random dockets for testing. A dev would run the script to download a bunch of random dockets, and then pytest will try parsing them. The UJS site is acting weird atm, but I _think_ the script works.

---
+++ RecordLib/number_generator.py
@@ -0,0 +1,35 @@
+import random
+import logging
+from types import GeneratorType
+
+
+def create_docket_numbers(court: str = "either") -> str:
+    """
+    Generate srandom docket numbers
+
+    Args:
+        num: the number of dockets numbers to create
+        court: CP or MD dockets? or "both" for both.
+    """
+    court = random.choice(["CP", "MDJ"]) if court == "either" else court
+    county_num = f"0{ random.randrange(1,68) }"[-2:]
+    docket_sequence = f"0000000{ random.randrange(1, 2000) }"[-7:]
+    year = random.randrange(2007, 2019)
+    if court == "CP":
+        court_type = random.choice(["CR", "MD"])
+        docket_number = f"CP-{ county_num }-{ court_type }-{ docket_sequence}-{ year }"
+        yield docket_number
+    if court == "MDJ":
+        # ct_office doesn't have to end in 101, but the offices that
+        # exist are different in each county. Might be up to 2-03 or
+        # or something else. But for testing, I think
+        # just generating random number from the 101 office will be
+        # fine.
+        ct_office = f"{ county_num }101"
+        docket_number = f"MJ-{ ct_office }-CR-{ docket_sequence }-{ year }"
+        yield docket_number
+
+    logging.warning(
+        "Odd. You didn't create any docket numbers. Did you select CP, MJ or either for the `court` parameter?"
+    )
+    yield ""
--- setup.py
+++ setup.py
@@ -10,5 +10,10 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
-    ]
+            "Click",
+    ],
+    entry_points='''
+        [console_scripts]
+        download_docs=scripts.download_dockets:cli
+    ''',
 )
