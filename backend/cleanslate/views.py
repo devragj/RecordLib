e2813c4ec0ec774cca9dd7ea2ba9cad63bf0d8f2
NateV <nathan.c.vogel@gmail.com>
2019-08-20 11:12:23 -0400

    Django rest (#41)
    * Fixed docker run command
    
    * Fixed docker run command
    
    * Add a Django REST backend
    
    This is the start of a Django REST backend.
    The backend is meant to be paired with a
    React frontend.  The Django app is in the
    directory "backend".   Currently, you can POST
    a Summary pdf file to localhost:8000/upload.
    The app will parse the file and return JSON with
    the defendant's information and a list of the cases
    from the file.

---
+++ backend/cleanslate/views.py
@@ -0,0 +1,58 @@
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework import status
+
+from RecordLib.crecord import CRecord
+from RecordLib.summary.pdf import parse_pdf
+from RecordLib.summary import Summary
+from RecordLib.analysis import Analysis
+from RecordLib.ruledefs import (
+    expunge_summary_convictions,
+    expunge_nonconvictions,
+    expunge_deceased,
+    expunge_over_70,
+)
+
+import json
+import os
+import os.path
+from datetime import *
+
+
+def serialize(obj):
+    """JSON serializer for objects not serializable by default json code"""
+
+    if isinstance(obj, date):
+        serial = obj.isoformat()
+        return serial
+
+    if isinstance(obj, time):
+        serial = obj.isoformat()
+        return serial
+
+    if isinstance(obj, timedelta):
+        serial = str(obj)
+        return serial
+
+    return obj.__dict__
+
+
+class FileUploadView(APIView):
+    # noinspection PyMethodMayBeStatic
+    def post(self, request, *args, **kwargs):
+        try:
+            pdf_file = request.data["file"]
+            rec = CRecord()
+            base = os.path.dirname(os.path.abspath(__file__))
+            tempdir = os.path.join(base, "tmp")
+            summary = parse_pdf(
+                pdf=pdf_file,
+                tempdir=tempdir)
+            rec.add_summary(summary)
+
+            json_to_send = json.dumps({"defendant": rec.person, "cases": rec.cases}, indent=4, default=serialize)
+            return Response(json_to_send, status=status.HTTP_200_OK)
+        except Exception as e:
+            print(e)
+            return Response("Something went wrong", status=status.HTTP_400_BAD_REQUEST)
+
