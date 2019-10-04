c910545b0fc6b633e5d24f4255fe9bcb52c04df5
Nate Vogel <nathan.c.vogel@gmail.com>
2019-10-04 13:24:39 -0400

    settings.py uses environment vars to look up the SECRET_KEY and DEBUG

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'b!rclajrlxc42ue%dgnyb4ca3vx!bl8vte!ha2nqq46uv1v)d6'
+SECRET_KEY = os.environ["SECRET_KEY"] 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG") == "TRUE"
 
 ALLOWED_HOSTS = []
 
