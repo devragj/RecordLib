2af9b34d8d5a4bdf06386cbf99b2bf77eededb67
Nate Vogel <nathan.c.vogel@gmail.com>
2019-10-07 12:13:34 -0400

    interface made for uploading multiple files

--- frontend/src/components/RecordUploader.js
+++ frontend/src/components/RecordUploader.js
@@ -1,12 +1,91 @@
-import React from "react";
+import React, { useState } from "react";
+import { connect } from "react-redux"
+import Container from "@material-ui/core/Container"
+import List from '@material-ui/core/List';
+import Button from "@material-ui/core/Button"
+import { makeStyles } from '@material-ui/core/styles';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from "@material-ui/core/ListItemText";
+
+import { fetchCRecord } from "../actions";
+/*
+*   Step in the Petition analysis and generation process for uploading files for processing.
+*
+*   This component is for uploading summary and docket files, sending them to the server, 
+*   and receiving the resulting CRecord. 
+*/
+
+const useStyles = makeStyles(theme => ({
+    root: {
+        flexGrow: 1,
+        maxWidth: 752,
+    },
+    demo: {
+        backgroundColor: theme.palette.background.paper,
+    },
+    title: {
+        margin: theme.spacing(4, 0, 2),
+    },
+}))
+
+// Helper component to show a list of the files selected.
+function FileList(props) {
+    const { selectedFiles } = props;
+    if (selectedFiles.length > 0) {
+        return(
+           <List>
+                {selectedFiles.map(f => {
+                    return(
+                        <ListItem>
+                            <ListItemText primary={f.name}/>
+                        </ListItem>
+                    )
+                })}
+           </List> 
+        )}
+    else {
+        return(
+            <div> No files selected yet </div>
+        )
+    }
+}
 
 
-
-
+// Component to select files and dispatch api call to upload them.
 function RecordUploader(props) {
+    const [selectedFiles, setSelectedFiles] = useState([]);
+
+    const onChangeHandler = event => {
+        setSelectedFiles([...event.target.files]);
+    }
+
+    const onClickHandler = () => {
+        fetchCRecord(selectedFiles);
+    };
+
+
     return(
-        <h1> Upload files </h1>
+        <Container> 
+            <h1> Upload files </h1>
+            <p> Select the summary and docket files you wish to analyze.</p>
+            <FileList selectedFiles={selectedFiles}/>
+            <div>
+                <input multiple type="file" name="file" onChange={onChangeHandler}/>
+                <Button onClick={onClickHandler}> Upload </Button>
+            </div>
+        </Container>
     )
 }
 
-export default RecordUploader
+
+function mapStateToProps(state) {
+    return {};
+};
+
+function mapDispatchToProps(dispatch) {
+    return { fetchCRecord: file => dispatch(fetchCRecord(file)) };
+};
+
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(RecordUploader)
