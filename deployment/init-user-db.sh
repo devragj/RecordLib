e9b3c201a56cb771f84c43267f8a2c2cd0ca4b3c
Nate Vogel <nathan.c.vogel@gmail.com>
2019-10-24 16:29:19 -0400

    fixed sql syntax error

--- deployment/init-user-db.sh
+++ deployment/init-user-db.sh
@@ -4,6 +4,6 @@
 set -e
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-    CREATE USER recordlib
-    CREATE USER recordlib
-    GRANT ALL PRIVILEGES ON DATABASE recordlib TO recordlib
+    CREATE USER recordlib;
+    CREATE USER recordlib;
+    GRANT ALL PRIVILEGES ON DATABASE recordlib TO recordlib;
