8740e2cccfcde3e3cbd5cbebb8020de5210b4f4b
Devra Garfinkle Johnson <devra.johnson@verizon.net>
2019-10-24 13:13:07 -0400

    Add a User navbar menu and logout functionality (#81)

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -8,6 +8,9 @@ import Toolbar from '@material-ui/core/Toolbar';
 import MenuIcon from '@material-ui/icons/Menu';
 import { Link as RouterLink } from 'react-router-dom';
 import Link from '@material-ui/core/Link'
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
+import { logout } from '../api';
 
 // The use of React.forwardRef will no longer be required for react-router-dom v6.
 // See https://github.com/ReactTraining/react-router/issues/6056
@@ -38,6 +41,20 @@ const useStyles = makeStyles(theme => ({
 
 function Navbar () {
     const classes = useStyles()
+    const [anchorEl, setAnchorEl] = React.useState(null);
+
+    const handleClick = event => {
+        setAnchorEl(event.currentTarget);
+    };
+
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
+
+    const localLogout = () => {
+        setAnchorEl(null);
+        logout()
+       };
 
     return (
         <div className={classes.root}>
@@ -51,6 +68,19 @@ function Navbar () {
                             Clean Slate Buddy
                         </Typography>
                     </Link>
+                    <Button aria-controls="simple-menu" color="inherit" aria-haspopup="true" onClick={handleClick}>
+                        User
+                    </Button>
+                    <Menu
+                        id="user-menu"
+                        anchorEl={anchorEl}
+                        keepMounted
+                        open={Boolean(anchorEl)}
+                        onClose={handleClose}
+                    >
+                        <MenuItem onClick={handleClose}>Profile</MenuItem>
+                        <MenuItem onClick={localLogout}>Logout</MenuItem>
+                    </Menu>
                     <Button color="primary" className={classes.buttonText} component={LinkToAbout} to="/about"> 
                         About
                     </Button>
