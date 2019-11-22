bcfb7ad3c7acd3e98676be6ec8eeca7b7ea53206
devragj <devra.johnson@verizon.net>
2019-11-22 08:56:35 -0500

    Handle Enter key in AddAlias

--- frontend/src/components/AddAlias.js
+++ frontend/src/components/AddAlias.js
@@ -14,10 +14,18 @@ function AddAlias(props) {
         setName("");
     }
 
+    const handleKeyDown = event => {
+        if (event.keyCode === 13) {
+            event.preventDefault();
+            event.stopPropagation();
+            handleClick();
+        }
+    }
+
     return (
         <div className="addAlias" >
            <span style={{marginLeft: "20px"}}>Alias: </span>
-           <input type="text" value={name} onChange={handleChange} />
+           <input type="text" value={name} onChange={handleChange} onKeyDown={handleKeyDown}/>
            <button type="button" style={{marginLeft: "20px"}} onClick={handleClick}>Add Alias</button>
         </div>
     );
